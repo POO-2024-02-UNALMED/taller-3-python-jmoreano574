5dd53612009dbe3f1c2dd615eb341c09681161ce
jmoreano574 <jmoreano@unal.edu.co>
2024-11-19 07:20:57 -0500

    Update control.py

--- televisores/control.py
+++ televisores/control.py
@@ -2,39 +2,39 @@ from __future__ import annotations
 from televisores.tv import TV
 
 class Control:
-    def __init__(self):
+    def __init__(self)->None:
         self._tv = None 
     
-    def enlazar(self, tv):
+    def enlazar(self, tv)->None:
         self._tv = tv
         tv.setControl(self)
 
-    def turnOn(self):
+    def turnOn(self)->None:
         self._tv.turnOn()
 
-    def turnOff(self,):
+    def turnOff(self,)->None:
         self._tv.turnOff()
 
-    def canalUp(self):
+    def canalUp(self)->None:
         self._tv.canalUp()
     
-    def canalDown(self):
+    def canalDown(self)->None:
         self._tv.canalDown()
 
-    def volumenUp(self):
+    def volumenUp(self)->None:
         self._tv.volumenUp()
 
-    def volumenDown(self):
+    def volumenDown(self)->None:
         self._tv.volumenDown()
 
-    def setCanal(self, canal: int):
+    def setCanal(self, canal: int)->None:
         self._tv.setCanal(canal)
 
-    def setVolumen(self, volumen: int):
+    def setVolumen(self, volumen: int)->None:
         self._tv.setVolumen(volumen)
 
     def getTv(self):
         return self._tv
     
-    def setTv(self, tv):
+    def setTv(self, tv)->None:
         self._tv = tv
